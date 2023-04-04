5f7d3e651dd3af134467d559a6cf73e3cae9412f
TechnoFm <129893248+TechnoFm@users.noreply.github.com>
2023-04-04 20:32:11 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+basic.showLeds(`
+    . # . . .
+    # # # . .
+    . # . . .
+    . . . . .
+    . . . # #
+    `)
+let nombre1 = 0
+let nombre2 = 0
